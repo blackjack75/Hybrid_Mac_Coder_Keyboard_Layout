7eb8afbc87c9510f4ba373589d47b059c23ea90a
Santiago Lema <info@smallte.ch>
2024-09-20 00:25:30 -0300

    added install script

---
+++ install_keymap.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+echo COPYING TO Library / Keyboard Layouts 
+
+cp -f ./Coder-Swiss-French.keylayout ~/Library/Keyboard\ Layouts/Coder-Swiss-French.keylayout
+
+echo YOU WILL NEED TO SELECT IT IN THE PREFS!
